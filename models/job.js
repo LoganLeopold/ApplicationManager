19d0044006425de9c53e2f2d57714d459d0efe08
Logan Leopold <loganleopld@me.com>
2019-01-28 12:18:21 -0500

    working on seeding + database testing/connection

---
+++ models/job.js
@@ -0,0 +1,25 @@
+const mongoose = require("../db/connection");
+const Schema = mongoose.Schema;
+
+const Job = new Schema ({
+
+    Title: String,
+    Location: String,
+    //trying to populate the pay with Employer's avg pay
+    Pay: {
+        type: Schema.Types.ObjectId,
+        ref: "Employer"
+    },
+    Company:
+        {
+          type: Schema.Types.ObjectId,
+          ref: "Employer"
+        },
+    Languages: [String],
+    ExpMin: Number,
+    ExpMax: Number,
+    RemoteOpt: String,
+
+})
+
+module.exports = mongoose.Schema('Job', Job)
