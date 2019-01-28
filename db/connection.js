13f48e30ef6b2f995637405c71b0e807990b1d1e
Logan Leopold <loganleopld@me.com>
2019-01-28 12:42:10 -0500

    deciding whether or not to seed

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,4 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/Application_Manager");
+mongoose.connect("mongodb://localhost:27017/Application_Manager");
 mongoose.Promise = Promise;
 module.exports = mongoose;
