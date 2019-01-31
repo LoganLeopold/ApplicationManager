c36c7fc6e2ded419251d26bb756b2051aefbe56e
Logan Leopold <loganleopld@me.com>
2019-01-31 11:01:53 -0500

    even though I learned my lesson yesterday I have been lean on commits - almost tried to run sass middlware but that will be a goal for a rebuild later - need minimum viable product

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
 const express = require('express')
 const parser = require('body-parser')
-// const static = require('express-static')
 const hbs = require('hbs')
 const methodOverride = require('method-override')
-// const seed = (require('./db/seed.js'))
-
 const app = express()
 
-
 app.use(methodOverride('_method'))
-// ^After a long time of working to establish an update route Stuart reminded me about methodoverride^
+// ^         ^          ^
+// After a long time of working to establish an update route Stuart reminded me about methodoverride^
 app.use(parser.urlencoded({extended: true}))
 app.set('view engine', 'hbs')
 app.use(parser.json())
 app.use(require('./routes/routes'))
 app.use(express.static('public'))
-// app.seed
-
-
 app.listen(3001, () => {
     console.log('listening on port 3001')
 })
