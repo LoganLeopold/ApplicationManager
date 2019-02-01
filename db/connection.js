09f25db6b0bd38bace27e01356c7b6bd7b4b08eb
Logan Leopold <loganleopld@me.com>
2019-02-01 09:56:41 -0500

    trying to run mongoose.connect as string

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 // ("mongodb://loganleopold:Stick2it!@ds219055.mlab.com:19055/application_manager",
 
 if (process.env.NODE_ENV == "production") {
-    mongoose.connect(process.env.MLAB_URL)
+    mongoose.connect(`${process.env.MLAB_URL}`)
   } else {
     mongoose.connect('mongodb://localhost/loganapplicationmanager');
   }
